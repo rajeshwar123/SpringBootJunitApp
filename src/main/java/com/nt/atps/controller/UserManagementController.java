d63ad923890e6f45f5278687531d4c971f061728
rajeshwar123 <rajgodwe@gmail.com>
2020-02-11 01:10:33 +0530

    commited by rajeshwar

--- src/main/java/com/nt/atps/controller/UserManagementController.java
+++ src/main/java/com/nt/atps/controller/UserManagementController.java
@@ -3,5 +3,11 @@ package com.nt.atps.controller;
 public class UserManagementController {
 	/*@autor
 	UserMangementServiec service;
+*
 */
+	public void m1() {
+		
+	}
+	
+	
 }
---
+++ src/main/java/com/nt/atps/utils/Constatns.java
@@ -0,0 +1,11 @@
+package com.nt.atps.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
+public class Constatns {
+	@Autowired
+	Environment env;
+	public static final Integer SERVER_PORT=6542;
+	
+}
