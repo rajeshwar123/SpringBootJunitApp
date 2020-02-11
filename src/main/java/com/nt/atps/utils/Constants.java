3524c5cddc40550b181c22bc9645a401dc219668
rajeshwar123 <rajgodwe@gmail.com>
2020-02-11 14:34:07 +0530

    Changes in Util package

--- src/main/java/com/nt/atps/utils/Constants.java
+++ src/main/java/com/nt/atps/utils/Constants.java
@@ -3,9 +3,10 @@ package com.nt.atps.utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 
-public class Constatns {
+public class Constants {
 	@Autowired
 	Environment env;
-	public static final Integer SERVER_PORT=6542;
-	
+	public static final Integer SERVER_PORT=6585;
+	public static final String APP_NAME="JUNIT";
+	public static final Integer val1=10;
 }
